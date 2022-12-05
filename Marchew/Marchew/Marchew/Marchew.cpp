64c9bae2b64b345bb9e397b6176dd3358a426cd6
Amarbach <amarbach@interia.pl>
2022-12-05 16:43:47 +0100

    okno

--- Marchew/Marchew/Marchew/Marchew.cpp
+++ Marchew/Marchew/Marchew/Marchew.cpp
@@ -1,8 +1,26 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include "include/GLFW/glfw3.h"
+#include "include/glad/glad.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "test1", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "error\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 }
 
