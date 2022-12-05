a79ab2aa35c57b9d30df72e657c270cca0a9310c
Amarbach <amarbach@interia.pl>
2022-12-05 16:56:30 +0100

    Window close

--- Marchew/Marchew/Marchew/Marchew.cpp
+++ Marchew/Marchew/Marchew/Marchew.cpp
@@ -1,6 +1,17 @@
 ﻿#include <iostream>
-#include "include/GLFW/glfw3.h"
 #include "include/glad/glad.h"
+#include "include/GLFW/glfw3.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_BACKSPACE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
 int main()
 {
@@ -9,18 +20,31 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "test1", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Marchew", NULL, NULL);
     if (window == NULL) {
         std::cout << "error\n";
         glfwTerminate();
         return -1;
     }
-
+    glfwMakeContextCurrent(window);
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    glfwMakeContextCurrent(window);
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
 }
 
